dcc8f86f824949149329bae8c19c2bf299b0fd02
ACE <acetip@aces-mbp.lab.lagcc>
2017-11-27 19:53:23 -0500

    search engine for gifs

--- js/script.js
+++ js/script.js
@@ -1,15 +1,34 @@
-fetch("https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dc6zaTOxFJmzC&limit=5")
-  .then(function(response) {
+
+var input = document.querySelector('#user-gif-search');
+var searchBtn = document.querySelector('#submit-gif-search');
+var gifContainer = document.querySelector('#gif-result-container');
+
+function findGifs(userInput){
+
+  fetch("https://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=dc6zaTOxFJmzC&limit=5")
+    .then(function(response) {
     return response.json();
-  })
-  .then(function(response){
+    })
+    .then(function(response){
     showGifs(response.data)
-  });
+    });
+} // closes find gif function
 
   function showGifs(data){
+    gifContainer.innerHTML = '';
     for (var x = 0; x < data.length; x++) {
       var img = document.createElement('img');
       img.src = data[x].images.original.url;
-      document.body.appendChild(img);
+      gifContainer.appendChild(img);
     }
   }
+if(input && searchBtn && gifContainer){
+  searchBtn.addEventListener('click', function(){
+    var userInput = input.value;
+      if(userInput !== ''){
+        findGifs(userInput);
+      }//else write something
+
+  })
+
+}
